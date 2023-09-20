a4fbbc6cbe4bbae47c36ba3c0c3a42a8a6ade824
naufaladrna08 <naufaladrn90@gmail.com>
2023-09-20 16:16:03 +0700

    added view data

--- client/src/components/OrderList.js
+++ client/src/components/OrderList.js
@@ -8,12 +8,16 @@ import OrderForm from './OrderForm'
 const OrderList = ({}) => {
   const orders = useContext(OrderContext)
   const setOrders = useContext(OrderContextUpdate)
+  const backendURL = axios.defaults.baseURL
 
   const [updatOrderData, setUpdateOrderData] = useState({})
   const [showUpdateModal, setShowUpdateModal] = useState(false)
+  const [showViewOrder, setShowViewOrder] = useState(false)
+  const [currentViewedOrder, setCurrentViewedOrder] = useState({})
 
-  const handleClose = () => setShowUpdateModal(false);
-  const handleShow = () => setShowUpdateModal(true);
+  const handleShow = () => setShowUpdateModal(true)
+  const handleClose = () => setShowUpdateModal(false)
+  const handleCloseView = () => setShowViewOrder(false)
 
   const getOrders = async () => {
     try {
@@ -23,6 +27,12 @@ const OrderList = ({}) => {
     } catch (err) {
       console.error(err.message)
     }
+  }
+  
+  const handleView = async (data) => {
+    setShowViewOrder(true)
+    setCurrentViewedOrder(data)
+    console.log(data)
   }
 
   const handleUpdate = async (data) => {
@@ -78,8 +88,9 @@ const OrderList = ({}) => {
               <td> {each.pickup_location} </td>
               <td> {each.dropoff_location} </td>
               <td>
-                <Button variant="primary" onClick={ (e) => handleUpdate(each) }> Update </Button>
-                <Button variant="danger" className="mx-2" onClick={ (e) => handleDelete(each.order_id) }> Delete </Button>
+                <Button variant="primary" size='sm' onClick={ (e) => handleView(each) }> View Data </Button>
+                <Button variant="primary" size='sm' onClick={ (e) => handleUpdate(each) }> Update </Button> 
+                <Button variant="danger"  size='sm' onClick={ (e) => handleDelete(each.order_id) }> Delete </Button>
               </td>
             </tr>
           ))) : (
@@ -96,6 +107,45 @@ const OrderList = ({}) => {
         </Modal.Header>
         <Modal.Body>
           <OrderForm type={ 'update' } data={ updatOrderData } />
+        </Modal.Body>
+      </Modal>
+
+      <Modal size='lg' show={ showViewOrder } onHide={ handleCloseView }>
+        <Modal.Header closeButton>
+          <Modal.Title> View Order </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="row mb-2">
+                <div className="col-md-4"> Car Name </div>
+                <div className="col-md-8"> { currentViewedOrder.car_name } </div>
+              </div>
+              <div className="row mb-2">
+                <div className="col-md-4"> Order Data </div>
+                <div className="col-md-8"> { currentViewedOrder.order_date } </div>
+              </div>
+              <div className="row mb-2">
+                <div className="col-md-4"> Pickup Date </div>
+                <div className="col-md-8"> { currentViewedOrder.pickup_date } </div>
+              </div>
+              <div className="row mb-2">
+                <div className="col-md-4"> Drop Off date </div>
+                <div className="col-md-8"> { currentViewedOrder.dropoff_date } </div>
+              </div>
+              <div className="row mb-2">
+                <div className="col-md-4"> Pickup Location </div>
+                <div className="col-md-8"> { currentViewedOrder.pickup_location } </div>
+              </div>
+              <div className="row mb-2">
+                <div className="col-md-4"> Drop Off Location </div>
+                <div className="col-md-8"> { currentViewedOrder.dropoff_location } </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <img src={ backendURL + "/" + currentViewedOrder.image } className="img-fluid" />
+            </div>
+          </div>
         </Modal.Body>
       </Modal>
     </>
